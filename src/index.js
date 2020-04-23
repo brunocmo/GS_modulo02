469f1d7a2c7bccae71b4a2967aecfd2d6b521b9a
Bruno Nunes <brunocmo.nunes@gmail.com>
2020-04-23 11:59:00 -0300

    Add properties

--- src/index.js
+++ src/index.js
@@ -1,15 +1,5 @@
-<<<<<<< HEAD
 import React from 'react'; 
 import { render } from 'react-dom';
 import App from './App';
 
 render(<App />, document.getElementById('app'));
-=======
-//TODO need to erase this from program for next lesson
-
-const soma = (a,b) => {
-  return a + b;
-}
-
-console.log(soma(10,3));
->>>>>>> 821ef9e5666e71719fb89e3495c3a1ba36acbcc7
